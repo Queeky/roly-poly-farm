29422530652a69df79aa426a8ed51393914393bd
Queeky <qamy.ava@gmail.com>
2024-05-10 12:50:05 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Roly-Poly Farm</title>
+</head>
+<body style="margin:0; padding:0;">
+    <h1 style="text-align:center; font-size:10vw;">WELCOME</h1>
+    <h3 style="text-align:center; font-size:2.5vw;">to the roly-poly farm</h3>
+    <div style="margin:0; padding:0; width:100vw; display:flex; align-items:center;">
+        <img style="width:20vw;" src="images/poly-1.jpg" alt="">
+        <img style="width:20vw;" src="images/poly-2.jpg" alt="">
+        <img style="width:20vw;" src="images/poly-3.jpg" alt="">
+        <img style="width:20vw;" src="images/poly-6.jpg" alt="">
+        <img style="width:20vw;" src="images/poly-4.jpg" alt="">
+        <img style="width:20vw;" src="images/poly-5.jpg" alt="">
+    </div>
+    <div style="display: flex; align-items: center; justify-content: center;">
+        <strong><p style="font-size: 2vw;">Here's a roly-poly fact: </p></strong>
+        // Place fact here
+    </div>
+</body>
+</html>
